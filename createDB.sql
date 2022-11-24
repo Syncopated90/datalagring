0e8bab6ee14077f3b49960526fbd89c14ff77515
Syncopated90 <ehne@kth.se>
2022-11-24 13:28:34 +0100

    student, instructor tables

--- createDB.sql
+++ createDB.sql
@@ -1,61 +1,28 @@
-CREATE TABLE "student"
+CREATE TABLE skill_levels
 (
-  "person_id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "person_number" varchar(12) UNIQUE,
-  "name" varchar(500),
-  "street" varchar(100),
-  "zip" varchar(5),
-  "city" varchar(50)
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  skill_level varchar(12)
 );
-
-CREATE TABLE "health_care_establishment"
+CREATE TABLE student
 (
-  "health_care_establishment_id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "HCE_id" varchar(10) UNIQUE,
-  "street" varchar(100),
-  "zip" varchar(5),
-  "city" varchar(50)
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  person_number varchar(12) NOT NULL UNIQUE,
+  name varchar(50) NOT NULL,
+  address varchar(50),
+  phone_number varchar(20) NOT NULL UNIQUE,
+  email varchar(50) NOT NULL UNIQUE,
+  contact_person INT NOT NULL,
+  skill_level_id INT NOT NULL ,
+  CONSTRAINT level_fk
+    FOREIGN KEY(skill_level_id)
+      REFERENCES skill_levels(id)
 );
-
-CREATE TABLE "doctor"
+CREATE TABLE instructor
 (
-  "doctor_id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "person_id" int NOT NULL REFERENCES "person",
-  "employment_id" varchar(10) UNIQUE NOT NULL
-);
-
-CREATE TABLE "visit"
-(
-  "visit_id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "time" timestamp,
-  "doctor_id" int NOT NULL REFERENCES "doctor",
-  "person_id" int NOT NULL REFERENCES "person",
-  "health_care_establishment_id" int NOT NULL REFERENCES "health_care_establishment"
-);
-
-CREATE TABLE "visit_purpose"
-(
-  "visit_id" int NOT NULL REFERENCES "visit" ON DELETE CASCADE,
-  "description" varchar(500) NOT NULL,
-  PRIMARY KEY("visit_id", "description")
-);
-
-CREATE TABLE "email"
-(
-  "email_id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "email" varchar(100) UNIQUE NOT NULL
-);
-
-CREATE TABLE "phone"
-(
-  "person_id" int NOT NULL REFERENCES "person" ON DELETE CASCADE,
-  "phone_no" varchar(12) NOT NULL,
-  PRIMARY KEY ("person_id", "phone_no")
-);
-
-CREATE TABLE "person_email"
-(
-  "email_id" int NOT NULL REFERENCES "email" ON DELETE CASCADE,
-  "person_id" int NOT NULL REFERENCES "person" ON DELETE CASCADE,
-  PRIMARY KEY("email_id", "person_id")
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  person_number varchar(12) NOT NULL UNIQUE,
+  name varchar(50) NOT NULL,
+  address varchar(50),
+  phone_number varchar(20) NOT NULL UNIQUE,
+  email varchar(50) NOT NULL UNIQUE
 );
