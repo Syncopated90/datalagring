b08fc9007dbce25c1ab40393f5dcce315c786663
Syncopated90 <ehne@kth.se>
2022-12-05 12:18:51 +0100

    possibly finished with query 4

--- queries.sql
+++ queries.sql
@@ -41,16 +41,38 @@ SELECT COUNT (*) AS students_without_siblings FROM student WHERE student.id NOT 
 -- shows the students without siblings:
 SELECT * FROM student WHERE student.id NOT IN ((SELECT student1_id FROM student_siblings) UNION (SELECT student2_id FROM student_siblings));
 
---select all instructors who have worked more than a certain number of shifts, in this case 6
+--select all instructors who have worked more than a certain number of shifts, in this case 6, query 3 in assignment
 SELECT instructor_id FROM ((SELECT instructor_id FROM group_lesson WHERE start_time BETWEEN '2022-12-01' AND '2022-12-31' )
 UNION ALL(SELECT instructor_id FROM individual_lesson WHERE start_time BETWEEN '2022-12-01' AND '2022-12-31' )
 UNION ALL(SELECT instructor_id FROM ensemble WHERE start_time BETWEEN '2022-12-01' AND '2022-12-31')) AS instructors
 GROUP BY instructor_id HAVING COUNT (*) > 1 ORDER BY COUNT(instructor_id) DESC;
+
 --count number of shifts for one instructor over the whole year
 SELECT COUNT (instructor_id) FROM ((SELECT instructor_id FROM group_lesson)
 UNION ALL(SELECT instructor_id FROM individual_lesson)
 UNION ALL(SELECT instructor_id FROM ensemble)) AS instructors
 WHERE instructor_id = 16;
 
+--lists number of ensemble participants
+EXPLAIN ANALYZE
+SELECT COUNT(student_id) AS participants, genre, start_time, CASE
+  WHEN COUNT(student_id) = maximum_participants THEN 'fully booked'
+  WHEN COUNT(student_id) = (maximum_participants - 1) THEN 'one free spot'
+  WHEN COUNT(student_id) = (maximum_participants - 2) THEN 'two free spots'
+  ELSE 'plenty of space'
+  END booking_situation
+FROM (SELECT * FROM ensemble
+INNER JOIN ensemble_participants
+ON ensemble.id = ensemble_participants.ensemble_id
+WHERE ensemble.start_time BETWEEN current_date AND current_date + interval '1 week') as events
+GROUP BY genre, start_time, maximum_participants ORDER BY genre, start_time;
+
 --updating minimum_participants in ensemble
 UPDATE ensemble SET minimum_participants=2;
+
+--finding current date and current month
+SELECT current_Date, (current_date + interval '1 week');
+SELECT date_trunc('month', current_date), date_trunc('month', (current_date + interval '1 month'));
+
+--finding ensembles within a certain date
+SELECT * FROM ensemble WHERE start_time BETWEEN current_date AND current_date + interval '1 week' ORDER BY genre, start_time ASC;
