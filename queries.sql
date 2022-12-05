85ff34f5dc9383aad66ccc7f809c1c3ab7775aab
Syncopated90 <ehne@kth.se>
2022-12-05 10:40:21 +0100

    added more ensemble participants

--- queries.sql
+++ queries.sql
@@ -42,12 +42,15 @@ SELECT COUNT (*) AS students_without_siblings FROM student WHERE student.id NOT 
 SELECT * FROM student WHERE student.id NOT IN ((SELECT student1_id FROM student_siblings) UNION (SELECT student2_id FROM student_siblings));
 
 --select all instructors who have worked more than a certain number of shifts, in this case 6
-SELECT instructor_id FROM ((SELECT instructor_id FROM group_lesson WHERE start_time BETWEEN '2022-02-01' AND '2022-02-28' )
-UNION ALL(SELECT instructor_id FROM individual_lesson WHERE start_time BETWEEN '2022-02-01' AND '2022-02-28' )
-UNION ALL(SELECT instructor_id FROM ensemble WHERE start_time BETWEEN '2022-02-01' AND '2022-02-28')) AS instructors
-GROUP BY instructor_id HAVING COUNT (*) > 2 ORDER BY COUNT(instructor_id) DESC;
---count number of shifts for one instructor
+SELECT instructor_id FROM ((SELECT instructor_id FROM group_lesson WHERE start_time BETWEEN '2022-12-01' AND '2022-12-31' )
+UNION ALL(SELECT instructor_id FROM individual_lesson WHERE start_time BETWEEN '2022-12-01' AND '2022-12-31' )
+UNION ALL(SELECT instructor_id FROM ensemble WHERE start_time BETWEEN '2022-12-01' AND '2022-12-31')) AS instructors
+GROUP BY instructor_id HAVING COUNT (*) > 1 ORDER BY COUNT(instructor_id) DESC;
+--count number of shifts for one instructor over the whole year
 SELECT COUNT (instructor_id) FROM ((SELECT instructor_id FROM group_lesson)
 UNION ALL(SELECT instructor_id FROM individual_lesson)
 UNION ALL(SELECT instructor_id FROM ensemble)) AS instructors
 WHERE instructor_id = 16;
+
+--updating minimum_participants in ensemble
+UPDATE ensemble SET minimum_participants=2;
